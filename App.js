108a61df0d3dbfb4e4d886509ff4f4cbe4ee8129
sflorio <sebastianflorio00@hotmail.com>
2021-05-15 23:51:14 -0300

    Fist commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [phoneNumber, setstate] = useState();
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Introduzca su numero de telefono</Text>
+      <TextInput value={phoneNumber} ></TextInput>
+      <Button title="Llamar" onPress={() => console.log(phoneNumber) } ></Button>
+
       <StatusBar style="auto" />
     </View>
   );
